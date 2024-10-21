38d6c3faf6e91e3b87e4ef56fdb401ff820e528d
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-21 15:32:26 -0500

    completed lab 1

---
+++ variable_examples.py
@@ -0,0 +1,27 @@
+# Create example variables with good names to represent the following items (make up a
+# sample value for each variable to contain):
+# ∗ Customer ID
+# ∗ Customer’s first, middle, and last name
+# ∗ Gender
+# ∗ Date of birth
+# ∗ Driver’s license number
+# ∗ Auto policy number
+# ∗ NOTE: Recall that you can create an empty string variable by setting the value to ' '
+# or a number variable with an initial value of 0
+# 3. Regarding the variables you creates, what assumptions did you make about the type of
+# value that the variable might need to contain? Make a note after the variable as an
+# inline code comment, starting with #.
+# 4. Create variables for your own name and the city and state where you were born
+
+customerID = '644381'
+firstName = 'Natalie'
+middleName = 'Alisson'
+lastName = 'Reyes'
+gender = 'Female'
+birthDate = '03/15/2005'
+driverLicenseNumber = '164862591475'
+autoPolicyNumber = '116629713486'
+
+birthName = 'Martin Reyes'
+birthCity = 'Cuenavaca'
+birthState = 'Morelos'
