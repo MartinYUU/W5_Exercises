85ec37b03fe13ddd843f87bcdc20c093ffb13dc6
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-21 14:19:35 -0500

    created hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+print('Hello World!')
+
+message = 'Hello World!'
+print(message)
+
+# Create two variables for dollars and cents:
+# ∗ dollars should contain a value of 3 dollars
+# ∗ cents should contain a value of 50 cents
+# ∗ Test your variables by running: print(dollars + cents)
+# If you get an incorrect result, adjust your variables and try again.
+# 8. BONUS: Create a piece of code with the strings '3 dollars' and 'fifty cents',
+# then combine them in a print statement that yields the text output 3 dollars fifty cents
+
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+sdollar = '3 dollars'
+scents =  'fifty cents'
+print(sdollar + ' ' + scents)
