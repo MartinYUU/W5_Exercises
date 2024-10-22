dbef5c99af6efdf46262f1e118711b1ec3a07544
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-22 11:50:26 -0500

    completed lab 2 q 3

---
+++ MathScripts/tip_amount.py
@@ -0,0 +1,7 @@
+# How do you calculate the tip amount on a restaurant bill given the tip percentage? 
+
+bill = 150.00
+tipPercent = 0.20
+tip = bill * tipPercent
+
+print('The tip on a $' + format(bill, ".2f"), 'restaurant bill is $' + format(tip, ".2f"))
