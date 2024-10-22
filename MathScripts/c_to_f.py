bf292beea9c00b3879dbbd50aefb2f837f707828
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-22 15:11:01 -0500

    completed lab 1 q 2

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,7 @@
+# 2. How do you convert a temperature from Celsius to Fahrenheit?
+
+celsius = 25
+fahrenheit = (celsius * 9.0 / 5.0) + 32
+
+
+print(format(celsius, ".2f") + ' degrees celsius is ' + format(fahrenheit, ".2f"), 'degress fahrenheit') 
