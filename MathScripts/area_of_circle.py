c71805b988033bb4bbd6d04baa556e407d622fef
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-22 11:58:59 -0500

    completed lab2 q 4

---
+++ MathScripts/area_of_circle.py
@@ -0,0 +1,7 @@
+import math
+# How do you calculate the area of a circle?
+
+radius = 10
+circArea = math.pi * (math.pow(radius, 2))
+
+print('The area of a circle with radius', radius, 'is', circArea)
