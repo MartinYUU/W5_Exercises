2c111043573bf0b81a9c67e4e85bc1d536aeaf92
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-22 11:00:34 -0500

    completed lab 2

--- variable_examples.py
+++ variable_examples.py
@@ -1,3 +1,4 @@
+# Lab 1
 # Create example variables with good names to represent the following items (make up a
 # sample value for each variable to contain):
 # ∗ Customer ID
@@ -27,3 +28,68 @@ autoPolicyNumber = '116629713486'
 birthName = 'Martin Reyes'
 birthCity = 'Cuenavaca'
 birthState = 'Morelos'
+#######################################################################################################################################
+# Lab 2
+
+
+# What is the full list of reserved words that can’t be used for variable names?
+# False
+# def
+# if
+# raise
+# None	
+# del
+# import
+# return
+# True	
+# elif
+# in
+# try
+# and
+# else
+# is
+# while
+# as	
+# except
+# lambda
+# with
+# assert
+# finally
+# nonlocal
+# yield
+# break
+# for
+# not
+# class
+# form
+# or
+# continue
+# global
+# pass
+
+
+
+
+# Pick 5 of these words and review the explanation for how it is used as a keyword in
+# Python. Add these 5 definitions as comments to your exercise document. Put ^^
+# around any terms that you are not familiar with
+
+
+# raise
+# The raise statement is used to raise an error. These errors are visible
+# in the traceback and they cancel the execution of the program if not handled properly.
+
+# None
+# There is no null value in Python. The None is an object that represents the 
+# absence of a value. It is like an empty object.
+
+# try
+# The try statement is used to make a try… except statement. The try statement 
+# starts a block of code that is tried to execute. If it fails, the except block catches the error.
+
+# lambda
+# A lambda function in Python is an anonymous function. It can take any number
+# of arguments but only have a single expression.
+
+# break
+# It is a control flow statement used to come out of a loop.
