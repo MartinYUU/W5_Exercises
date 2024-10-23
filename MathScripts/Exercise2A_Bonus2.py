be51b9a73a1f422f7b0be92255048b3924f9888a
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-23 14:31:28 -0500

    completed exercise 2A Bonus 2

---
+++ MathScripts/Exercise2A_Bonus2.py
@@ -0,0 +1,9 @@
+# I will be rewriting the tip_amount exercise to complete this bonus exercise
+
+
+bill = 150.00
+tipPercent = 0.20
+tip = bill * tipPercent
+
+print (f'The tip on a ${bill:.2f} restaurant bill is ${tip:.2f}')
+print (f'The total due will be ${bill + tip}')
