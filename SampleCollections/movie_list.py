c3317a90fabf84bf24185239e85566da624bbbaa
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-23 16:18:40 -0500

    completed exercise 3b lab1

--- SampleCollections/movie_list.py
+++ SampleCollections/movie_list.py
@@ -1,4 +1,17 @@
 myFavMovies = ['Inception', 'Shutter Island', 'Cars', 'Insidius']
 
+print()
 print('The list myFavMovies include my top', len(myFavMovies), 'favorite movies.')
 print(myFavMovies)
+print()
+
+print(sorted(myFavMovies))
+print(myFavMovies)
+print()
+
+myFavMovies.sort()
+print(myFavMovies)
+
+# Sorted() did not change the order of the list assigned the original variable
+# which is why when you print the list again, the list is not sorted
+# .sort() sorted the list and changed the order in the original variable
