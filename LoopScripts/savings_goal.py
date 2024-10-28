3d4e773229a0dc5edcde349e65c6b8c0226b2cd0
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-28 16:39:46 -0500

    actually completed exercise 4C lab 2

--- LoopScripts/savings_goal.py
+++ LoopScripts/savings_goal.py
@@ -6,13 +6,28 @@
 # print the statement, “This week my balance increased to ___.” Once your savings goal 
 # is met, print the statement, “Goal met! My current balance is ___.” 
 
+# Try adding additional logic to your loop:  
+# ∗ If your balance is more than halfway to your goal, print the message, “Almost there! 
+# This week my balance is up to ___.” 
+# ∗ If your balance is at least 75% of your goal, add a calculation to buy yourself a little 
+# treat. Print the message, “So close! After treating myself, my balance is up to ___.”
+
 
 starting_balance = 1000
 savings_goal = 5000
 weekly_savings_amt = 120
+treat_cost = 20
 
 while starting_balance < savings_goal:
     starting_balance += weekly_savings_amt
-    print('This week my balace is $' + format(starting_balance, ".2f"))
+    if starting_balance <= savings_goal / 2:
+        print('This week my balace is $' + format(starting_balance, ".2f"))
+    elif starting_balance > savings_goal / 2 and starting_balance < savings_goal * 0.75:
+        print('Almost there! This week my balace is uo to $' + format(starting_balance, ".2f"))
+    elif starting_balance >= savings_goal * 0.75:
+        starting_balance -= treat_cost
+        print('So close! After treating myself, my balance is up to $' + format(starting_balance, ".2f"))
+
+
 
 print('Goal met! My current balance is ' + format(starting_balance, ".2f"))
